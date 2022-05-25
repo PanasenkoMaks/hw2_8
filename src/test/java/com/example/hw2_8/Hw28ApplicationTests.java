7177c4a86e194f2b59566778988c3b35619b5da9
PanasenkoMaks <panasenko_maks@mail.ru>
2022-05-25 17:06:01 +0500

    hw2_8

---
+++ src/test/java/com/example/hw2_8/Hw28ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hw2_8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw28ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
